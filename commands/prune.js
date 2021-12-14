6c611e7c072707660f5aa9b964f90448fb491309
Levi Ross <levi.ross@okstate.edu>
2021-12-13 20:22:22 -0600

    prune test

--- commands/prune.js
+++ commands/prune.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pruneTest')
+		.setName('prunetest')
 		.setDescription('Prune up to 99 messages.')
 		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to prune')),
 	async execute(interaction) {
