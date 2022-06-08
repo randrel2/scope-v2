b9b1aa3232e9c92493597ca1e6416c4e95da4c2f
CTFries <me@ctfries.dev>
2022-06-07 22:44:57 -0400

    troubleshooting applied now that its functional

--- commands/dad.js
+++ commands/dad.js
@@ -3,8 +3,7 @@ const got = require('got');
 module.exports = {
   data: new SlashCommandBuilder().setName("dad").setDescription("jokes"),
   async execute(interaction) {
-    const {data} = await got.get('https://icanhazdadjoke.com/', {
-  }).json();
+    const data = await got.get('https://icanhazdadjoke.com/').json()
     return await interaction.reply(data.joke);
   },
 };
