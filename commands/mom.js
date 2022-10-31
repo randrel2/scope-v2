00fec590f91e858e8d36f43dbcc2f6bbaf2777e6
ctfries <me@ctfries.dev>
2022-10-31 16:43:03 -0400

    changes source for more jokes

--- commands/mom.js
+++ commands/mom.js
@@ -1,9 +1,15 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const got = require("got");
+const { jokes } = require("../resources/mamajokes.js");
 module.exports = {
   data: new SlashCommandBuilder().setName("mom").setDescription("jokes"),
   async execute(interaction) {
-    const data = await got.get("https://api.yomomma.info/").json();
-    return await interaction.reply(data.joke);
+    const sortedCategories = Object.keys(jokes).sort();
+    var category =
+      jokes[
+        sortedCategories[Math.floor(Math.random() * Object.keys(jokes).length)]
+      ];
+    const joke = category[Math.floor(Math.random() * category.length)];
+    return await interaction.reply(joke);
   }
 };
